a820a06b0b1be73a2bdef7e2c7ce203279a1a547
Björn Johanson <bjorjoh.dev@gmail.com>
2018-02-13 06:27:17 +0100

    added fake circular behaviour

--- 2017/one/one.go
+++ 2017/one/one.go
@@ -5,21 +5,45 @@ import (
 	"io/ioutil"
 )
 
-func Run(){
+func Run() {
 	fmt.Println("hello from package")
-	//getTestData("")
+	data, err := getTestData("/one/data/input.txt")
+	checkErr(err)
+	getSum(data)
 
 }
+func getSum(bytes []byte) int {
 
-func getTestData(path string) ([]byte,error){
+	stringData := string(bytes)
+	var sum int
+	var cmp int
+	for i, r := range stringData {
+		if i != len(bytes)-1 {
+			cmp = int(stringData[i+1] - '0')
+		} else {
+			cmp = int(stringData[0] - '0')
+		}
+		if int(r-'0') == cmp {
+			sum += int(r - '0')
+		}
+	}
+
+	return sum
+}
+
+func getTestData(path string) ([]byte, error) {
 	dat, err := ioutil.ReadFile(path)
 	checkErr(err)
 
-	return dat,err
+	return dat, err
 }
 
-func checkErr(e error){
-	if e != nil{
+func checkErr(e error) {
+	if e != nil {
 		fmt.Println(e)
 	}
+}
+
+func nextIsMatch(data []byte) {
+
 }
--- 2017/one/one_test.go
+++ 2017/one/one_test.go
@@ -1,22 +1,32 @@
 package one
 
 import (
-	"testing"
 	"fmt"
 	"strings"
-	)
+	"testing"
+)
 
-func TestGetTestData(t *testing.T){
-	data,err := getTestData("data/testData.txt")
+func TestGetTestData(t *testing.T) {
+	data, err := getTestData("data/testDataOne.txt")
 	fmt.Println(string(data))
 
-	expected := "123425"
+	expected := "1122"
 
+	if err != nil {
+		t.Errorf("Unable to read testDataOne%s \n", err)
+	} else if res := strings.Compare(string(data), expected); res != 0 {
+		t.Errorf("Error while reading testData. Got: %s Expected: %s", string(data), expected)
+	}
 
-	if err != nil{
-		t.Errorf("Unable to read testData%s \n",err)
-	} else if res := strings.Compare(string(data),expected); res != 0{
-		t.Errorf("Error while reading testData. Got: %s Expected: %s",string(data),expected)
+}
+
+func TestGetSum(t *testing.T) {
+
+	data, _ := getTestData("data/testDataOne.txt")
+	expected := 4
+
+	if res := getSum(data); res != expected {
+		t.Errorf("Error while getting sum. Got: %d Expected: %d\n", res, expected)
 	}
 
 }
