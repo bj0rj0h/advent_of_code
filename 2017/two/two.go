70becbbf56c3e0a7cd3937a19a38ec9167af7652
Björn Johanson <bjorjoh.dev@gmail.com>
2018-02-17 20:07:16 +0100

    changed matrix to store int instead of string

--- 2017/two/two.go
+++ 2017/two/two.go
@@ -2,7 +2,7 @@ package two
 
 import (
 	"io/ioutil"
-	"fmt"
+	"strconv"
 )
 
 func Run(){
@@ -15,36 +15,38 @@ func getData(path string) ([]byte,error){
 	return dat, err
 }
 
-func bytesToSlice(bytes []byte) [][]string {
-	data := make([][]string, 0)
-	inner := make([]string,0)
+func bytesToSlice(bytes []byte) [][]int {
+	data := make([][]int, 0)
+	inner := make([]int,0)
 	s := ""
 	prevSpace := false
 	for _,b := range bytes{
 		if b == 32 || b == 9{
 			if !prevSpace {
 				prevSpace = true
-				inner = append(inner, s)
+				intS,err := strconv.Atoi(s)
+				checkErr(err)
+				inner = append(inner, intS)
 				s = ""
 			}
 		}else if b == 10 {
 			prevSpace = false
-			inner = append(inner,s)
+			intS,err := strconv.Atoi(s)
+			checkErr(err)
+			inner = append(inner, intS)
 			s = ""
 			data = append(data,inner)
-			inner = make([]string,0)
-
+			inner = make([]int,0)
 		}else {
 			prevSpace = false
 			s += string(b)
 		}
-
 	}
 	return data
 }
 
 func checkErr(e error) {
 	if e != nil {
-		fmt.Println(e)
+		panic(e)
 	}
 }
--- 2017/two/two_test.go
+++ 2017/two/two_test.go
@@ -2,20 +2,42 @@ package two
 
 import (
 	"testing"
-	"fmt"
 )
 
 func TestGetData(t *testing.T){
 	data,_ := getData("testData.txt")
-	fmt.Println(data)
+	var sum int
+	expected := 1158
+	for _,val := range data{
+		sum += int(val)
+	}
+	if sum != expected{
+		t.Errorf("Wrong data read from file. Got: %d expected: %d",sum,expected)
+	}
 }
 
 func TestBytesToSlice(t *testing.T){
-	data,_ := getData("input.txt")
-	fmt.Println(data)
-	slice := bytesToSlice(data)
-	fmt.Println(slice)
+	data,_ := getData("testData.txt")
+	slices := bytesToSlice(data)
+	var sum int
+	expected := 198
+	for _,slice := range slices{
+		for _,s := range slice{
+			sum += s
+		}
+	}
+	if sum != expected{
+		t.Errorf("Wrong data read from file. Got: %d expected: %d",sum,expected)
+	}
 }
+
+func TestCalcDiff(t *testing.T){
+	/*diff := calcDiff(slice []string){
+
+	}*/
+}
+
+
 
 
 
