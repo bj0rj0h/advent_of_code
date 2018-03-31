4f0d5204d8676ee8771ce3913b4550983bd062c7
Björn Johanson <bjorjoh.dev@gmail.com>
2018-03-31 09:52:25 +0200

    added algorithm for finding cordinates of the value that we want to step to

--- 2017/three/three.go
+++ 2017/three/three.go
@@ -6,31 +6,117 @@ import (
 
 type State struct {
 	direction rune
-	steps int
+	steps     int
 }
 
-func Run(){
+func Run() {
 
 }
 
-func generateGrid(max int)[][]int{
-	numSquares := calcGridSize(max,0,1)
-	fmt.Println("NumSquares: %d",numSquares)
-	return make([][]int, numSquares*2+1)
+func generateGrid(max int) [][]int {
+	numSquares := calcGridSize(max, 0, 1)
+	fmt.Printf("NumSquares: %d\n", numSquares)
+	grid := make([][]int, numSquares*2+1)
+	for i := range grid {
+		grid[i] = make([]int, numSquares*2+1)
+	}
+	return grid
 }
 
-func calcGridSize(neededMaxVal,gridSize,gridMax int) int{
+func populateGrid(desiredValue int, maxVal int, grid [][]int) (int, int) {
+	return firstHalf(desiredValue, maxVal, grid)
+}
+
+func firstHalf(desiredValue int, value int, grid [][]int) (int, int) {
+
+	var qx, qy int
+
+	if value == 1 {
+		return (len(grid) - 1) / 2, (len(grid) - 1) / 2
+	}
+
+	for gridSize := len(grid) - 1; gridSize >= (len(grid)-1)/2; gridSize-- {
+
+		//bottom row
+		firstRowIndex := (len(grid) - 1) - gridSize
+		bottomRow := grid[firstRowIndex]
+		for x := (len(bottomRow) - 1) - firstRowIndex; x > (0 + firstRowIndex); x-- {
+			if value == desiredValue {
+				qx = x
+				qy = firstRowIndex
+			}
+			bottomRow[x] = value
+			value--
+		}
+		grid[firstRowIndex] = bottomRow
+		//leftRow
+		for y := 0 + firstRowIndex; y < (len(grid)-1)-firstRowIndex; y++ {
+			grid[y][0+firstRowIndex] = value
+			if value == desiredValue {
+				qx = firstRowIndex
+				qy = y
+			}
+			value--
+		}
+
+		//topRow
+		topRow := grid[(len(grid)-1)-firstRowIndex]
+		for x := 0 + firstRowIndex; x < (len(grid)-1)-firstRowIndex; x++ {
+			topRow[x] = value
+			if value == desiredValue {
+				qx = x
+				qy = (len(grid) - 1) - firstRowIndex
+			}
+			value--
+		}
+
+		//rightRow
+		for y := (len(grid) - 1) - firstRowIndex; y >= (0 + (len(grid) - 1) - gridSize + 1); y-- {
+			grid[y][(len(grid)-1)-firstRowIndex] = value
+			if value == desiredValue {
+				qx = (len(grid) - 1) - firstRowIndex
+				qy = y
+			}
+			value--
+		}
+
+	}
+
+	return qx, qy
+}
+
+func secondHalf(grid [][]int) [][]int {
+	return grid
+}
+
+func calcGridSize(neededMaxVal, gridSize, gridMax int) int {
 
 	if gridMax < neededMaxVal {
-		gridMax = 8*(gridSize+1)+gridMax
-		return calcGridSize(neededMaxVal,gridSize+1,gridMax)
+		gridMax = 8*(gridSize+1) + gridMax
+		return calcGridSize(neededMaxVal, gridSize+1, gridMax)
 	}
-		return gridSize
+	return gridSize
 }
 
-func calculateWalkingDistance(startVal int, grid [][]int) int{
-
-
+func calculateWalkingDistance(startVal int, grid [][]int) int {
+	//var q1, q2 = getQPos(startVal, grid)
 
 	return 0
+
+}
+
+func getQPos(startVal int, grid [][]int) (int, int) {
+
+	var q1, q2 int
+
+	for i, e := range grid {
+		for j, je := range e {
+			if je == startVal {
+				q1, q2 = i, j
+			}
+		}
+	}
+
+	return q1, q2
+
 }
--- 2017/three/three_test.go
+++ 2017/three/three_test.go
@@ -1,36 +1,78 @@
 package three
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestGenerateGrid(t *testing.T){
+func TestGenerateGrid(t *testing.T) {
 	grid := generateGrid(23)
 	expected := 5
-	if expected != len(grid){
-		t.Errorf("Expected: %d Got: %d",expected, len(grid))
+	if expected != len(grid) {
+		t.Errorf("Expected: %d Got: %d", expected, len(grid))
 	}
 	grid = generateGrid(26)
 	expected = 7
-	if expected != len(grid){
-		t.Errorf("Expected: %d Got: %d",expected, len(grid))
+	if expected != len(grid) {
+		t.Errorf("Expected: %d Got: %d", expected, len(grid))
 	}
 }
 
-func TestCalcGridSize(t *testing.T){
-	result := calcGridSize(23,0,1)
+func TestCalcGridSize(t *testing.T) {
+	result := calcGridSize(23, 0, 1)
 	expected := 2
 	if expected != result {
-		t.Errorf("Expected: %d Got: %d",expected, result)
+		t.Errorf("Expected: %d Got: %d", expected, result)
 	}
 }
 
-func TestCalculateWalkingDistance(t *testing.T){
-	startVal := 23
-	grid:= generateGrid(startVal)
-	result := calculateWalkingDistance(startVal,grid)
-	expected := 2
+func TestCalculateWalkingDistance_1_PASS(t *testing.T) {
+	startVal := 1
+	grid := generateGrid(startVal)
+	result := calculateWalkingDistance(startVal, grid)
+	expected := 0
 	if expected != result {
-		t.Errorf("Expected: %d Got: %d",expected, result)
+		t.Errorf("Expected: %d Got: %d", expected, result)
 	}
+}
+
+func TestCalculateWalkingDistance_12_PASS(t *testing.T) {
+	startVal := 12
+	grid := generateGrid(startVal)
+	result := calculateWalkingDistance(startVal, grid)
+	expected := 3
+	if expected != result {
+		t.Errorf("Expected: %d Got: %d", expected, result)
+	}
+}
+
+func TestGetQPost(t *testing.T) {
+
+	startVal := 12
+	grid := generateGrid(startVal)
+	q1, q2 := getQPos(startVal, grid)
+
+	expectedQ1, expectedQ2 := 4, 3
+
+	if q1 != expectedQ1 {
+		t.Errorf("Expected q1 to be: %d Got: %d", expectedQ1, q1)
+	}
+	if q2 != expectedQ2 {
+		t.Errorf("Expected q2 to be: %d Got: %d", expectedQ2, q2)
+	}
+}
+
+func TestPopulateGrid(t *testing.T) {
+	grid := generateGrid(12)
+	expectedQ1, expectedQ2 := 4, 3
+	q1, q2 := populateGrid(12, 25, grid)
+
+	if q1 != expectedQ1 {
+		t.Errorf("Expected q1 to be: %d Got: %d", expectedQ1, q1)
+	}
+	if q2 != expectedQ2 {
+		t.Errorf("Expected q2 to be: %d Got: %d", expectedQ2, q2)
+	}
+
+	fmt.Println("done")
 }
